78ca15be00f2be8fc770f50e097953fe0ca81710
Haile Yakob <haileyakob@gmail.com>
2020-09-26 16:32:44 +0300

    Add files via upload

--- purchase_request/models/stock_picking.py
+++ purchase_request/models/stock_picking.py
@@ -113,3 +113,33 @@ class StockPicking(models.Model):
                     subtype="mail.mt_comment",
                     author_id=self.env.user.partner_id.id,
                 )
+
+class ReturnPickingUpdate(models.TransientModel):
+    _inherit = 'stock.return.picking'
+
+    @api.onchange('picking_id')
+    def _onchange_picking_id(self):
+        move_dest_exists = False
+        product_return_moves = [(5,)]
+        if self.picking_id and (self.picking_id.state != 'done' or self.picking_id.state != 'received'):
+            from odoo.exceptions import UserError
+            raise UserError(_("You may only return Done pickings."))
+        for move in self.picking_id.move_lines:
+            if move.state == 'cancel':
+                continue
+            if move.scrapped:
+                continue
+            if move.move_dest_ids:
+                move_dest_exists = True
+            product_return_moves.append((0, 0, self._prepare_stock_return_picking_line_vals_from_move(move)))
+        if self.picking_id and not product_return_moves:
+            raise UserError(_("No products to return (only lines in Done state and not fully returned yet can be returned)."))
+        if self.picking_id:
+            self.product_return_moves = product_return_moves
+            self.move_dest_exists = move_dest_exists
+            self.parent_location_id = self.picking_id.picking_type_id.warehouse_id and self.picking_id.picking_type_id.warehouse_id.view_location_id.id or self.picking_id.location_id.location_id.id
+            self.original_location_id = self.picking_id.location_id.id
+            location_id = self.picking_id.location_id.id
+            if self.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.return_location:
+                location_id = self.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.id
+            self.location_id = location_id
